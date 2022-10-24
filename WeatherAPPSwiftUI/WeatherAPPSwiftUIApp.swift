20b200859069652a8baec33872063c04e93c5251
Darina Melis <darina05@email.cz>
2022-10-24 14:40:22 +0200

    Initial Commit

---
+++ WeatherAPPSwiftUI/WeatherAPPSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPPSwiftUIApp.swift
+//  WeatherAPPSwiftUI
+//
+//  Created by Daryna Polevyk on 24.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPPSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
